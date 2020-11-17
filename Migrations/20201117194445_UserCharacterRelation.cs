4053883198590cdbb9936d09f59a9858f93599e0
engrceey <ogbondachristian@gmail.com>
2020-11-17 22:07:15 +0100

    add migration

---
+++ Migrations/20201117194445_UserCharacterRelation.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Learningcsharp.Migrations
+{
+    public partial class UserCharacterRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsersId",
+                table: "Characters",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_UsersId",
+                table: "Characters",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Users_UsersId",
+                table: "Characters",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Users_UsersId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_UsersId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "UsersId",
+                table: "Characters");
+        }
+    }
+}
